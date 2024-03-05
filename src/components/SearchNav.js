7efc4a4ae181a214ccfb601e02f74830ebe5c491
vamsi <chandravamsi.mothadi@gmail.com>
2024-03-05 20:04:35 +0530

    added search bar

--- src/components/SearchNav.js
+++ src/components/SearchNav.js
@@ -1,12 +1,19 @@
 import React from 'react'
 
 const SearchNav = () => {
+  
   return (
     <div className='search-nav'>
-            <div className="dummy">
-      Location
+        <div className="dummy">
+             Location
       <i className="fa-solid fa-location"></i>
       </div>
+      <div className="search-bar"><input className="input" placeholder="Search"></input>
+       <div>
+          <button className="search-button"><i class="fa-solid fa-magnifying-glass"></i></button>
+       </div>
+      </div>
+      <div></div>
     </div>
   )
 }
